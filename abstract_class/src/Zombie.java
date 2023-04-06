28c3917d4abd6b0c9f48cb02d7a8339084ca359d
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-04-06 17:44:13 +0200

    abstrat_class_constructor_in_non_abstract_class.png

--- abstract_class/src/Zombie.java
+++ abstract_class/src/Zombie.java
@@ -17,8 +17,14 @@ public class Zombie extends Human{
         System.out.println(getB());
     }
 
+    public void displayFromHumanClass(){
+        System.out.println(getA());
+    }
 
-    public Zombie(int a, int b, int c, int d, int i, int j, int k, int l) {
+
+    public Zombie(int a, int b, int c, int d,
+                  int i, int j, int k, int l)
+    {
         super(a, b, c, d, i, j, k, l);
     }
 }
