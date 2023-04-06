aa8f52aed60a7bf563afe8cb8771a5d556598f54
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-04-06 18:03:32 +0200

    png

--- abstract_class/src/Main.java
+++ abstract_class/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Zombie zombie = new Zombie(1,2,3,4,5,6,7,8);
+        System.out.println(zombie.getA());
         System.out.println(zombie.getB());
     }
 }
