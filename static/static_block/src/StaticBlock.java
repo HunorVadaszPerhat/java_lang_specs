c00be823c6c666d0d1f118a4b4d2cc45e24d96df
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-04-08 11:11:53 +0200

    static_block.png

--- static/static_block/src/StaticBlock.java
+++ static/static_block/src/StaticBlock.java
@@ -1,6 +1,5 @@
 public class StaticBlock {
     static int a;
-
     // static blocks can be used to initialize fields
     // static blocks are called when classes are loaded
     static {
@@ -8,7 +7,6 @@ public class StaticBlock {
         a = 10;
         System.out.println("Static block no.1 -> value of a is: " + a);
     }
-
     static {
         System.out.println("Static block no.2 - initialize static variable a.");
         a = 20;
