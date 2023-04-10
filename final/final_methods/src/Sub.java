a7871671c9661910abc5dc3ec5658b33dfa83245
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-04-10 10:56:05 +0200

    final_methods.png

--- final/final_methods/src/Sub.java
+++ final/final_methods/src/Sub.java
@@ -1,6 +1,6 @@
 public class Sub extends Super {
     // final methods CANNOT BE OVERRIDEN by sub-classes
-    // void method1(){}; -> 'method1()' cannot override 'method1()' in 'Super'; overridden method is final
+    void method1(){};
 
     void method2(){
         System.out.println("Sub class : non-final method2");
