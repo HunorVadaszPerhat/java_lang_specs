1e78623955acef2c489426564cbcf897e173e5ad
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-04-12 16:38:48 +0200

    method_hide_4.png

---
+++ method_hiding/method_hiding_4/src/BMW.java
@@ -0,0 +1,5 @@
+public class BMW extends Car {
+    public static void methodA() {
+        System.out.println("static methodA() from subclass");
+    }
+}
---
+++ method_hiding/method_hiding_4/src/Car.java
@@ -0,0 +1,5 @@
+public class Car {
+    public static void methodA() {
+        System.out.println("static methodA() from superclass");
+    }
+}
